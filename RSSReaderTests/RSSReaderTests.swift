296d75075ee42e3768e6e6cb92f92e1b8b01620a
Alberto Barrago <albertobarrago@gmail.com>
2025-08-28 16:49:32 +0200

    Initial Commit

---
+++ RSSReaderTests/RSSReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  RSSReaderTests.swift
+//  RSSReaderTests
+//
+//  Created by Alberto Barrago on 28/08/25.
+//
+
+import Testing
+@testable import RSSReader
+
+struct RSSReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
