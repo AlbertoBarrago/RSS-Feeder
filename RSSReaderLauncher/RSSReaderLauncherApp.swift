6e5a73ce219b0f94683a2a82e60568a53b398a16
Alberto Barrago <albertobarrago@gmail.com>
2025-09-05 13:01:53 +0200

    Feat: Added run on start and notifications (close #9)

---
+++ RSSReaderLauncher/RSSReaderLauncherApp.swift
@@ -0,0 +1,34 @@
+
+import AppKit
+import os.log
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let mainAppIdentifier = "com.example.RSSReader"
+        let runningApps = NSWorkspace.shared.runningApplications
+        let isRunning = !runningApps.filter { $0.bundleIdentifier == mainAppIdentifier }.isEmpty
+
+        if !isRunning {
+            let path = Bundle.main.bundlePath as NSString
+            var components = path.pathComponents
+            components.removeLast()
+            components.removeLast()
+            components.removeLast()
+            components.append("MacOS")
+            components.append("RSSReader")
+
+            let newPath = NSString.path(withComponents: components)
+
+            NSWorkspace.shared.launchApplication(newPath)
+        }
+        else {
+            os_log("RSSReader is already running")
+        }
+        NSApp.terminate(nil)
+    }
+}
+
+let app = NSApplication.shared
+let delegate = AppDelegate()
+app.delegate = delegate
+app.run()
