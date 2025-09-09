6b7aea33592b3cf6893297d65c0a73977b20d8f8
Alberto Barrago <albertobarrago@gmail.com>
2025-09-09 11:10:40 +0200

    (close #18)

--- RSSReaderLauncher/RSSReaderLauncherApp.swift
+++ RSSReaderLauncher/RSSReaderLauncherApp.swift
@@ -4,12 +4,9 @@ import os.log
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-<<<<<<< HEAD
-        let mainAppIdentifier = "com.example.RSSReader"
-||||||| 6e6c5d8
-=======
+
         let mainAppIdentifier = "com.albz.RSSReader"
->>>>>>> develop
+
         let runningApps = NSWorkspace.shared.runningApplications
         let isRunning = !runningApps.filter { $0.bundleIdentifier == mainAppIdentifier }.isEmpty
 
